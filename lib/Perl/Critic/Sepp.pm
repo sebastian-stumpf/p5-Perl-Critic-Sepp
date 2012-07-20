73ec3ff12034510f4dbc3f5a601aead9874ca8a5
Sebastian Stumpf <git@perlhacker.org>
2012-07-20 17:06:46 +0200

    initial commit

---
+++ lib/Perl/Critic/Sepp.pm
@@ -0,0 +1,34 @@
+package Perl::Critic::Sepp;
+use strict;
+use warnings;
+use autodie;
+# ABSTRACT: A short description of Perl::Critic::Sepp
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Perl::Critic::Sepp - 
+
+=head1 SYNOPSIS
+
+  
+  
+
+=head1 DESCRIPTION
+
+=head1 EXAMPLE
+
+=head1 AUTHOR
+
+Sebastian Stumpf E<lt>sepp@perlhacker.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
